5192a05b9a28c3986d44ed596781c1032df65e71
Mahdi Saidi <mahdisaidi0406@gmail.com>
2025-02-26 12:21:51 +0100

    W2 My first React component

---
+++ my-react-project/src/App.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+function getTitle(title){
+  return title;
+}
+
+
+
+function App(){
+  return(
+    <div>
+      <h1> Hello {getTitle("React")}</h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text"/>
+    </div>
+  );
+}
+export default App;
